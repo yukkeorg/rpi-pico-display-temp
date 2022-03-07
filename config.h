de6d5d2ad8650bd80c7e146992779be36a24e864
Ohshima Yusuke <ohshima.yusuke@yukke.org>
2022-03-07 22:35:27 +0900

    Add SO1602A_CONFIG.

--- config.h
+++ config.h
@@ -4,6 +4,7 @@
 #define ADT7410_CONFIG    (0x80)
 #define ADT7410_I2C_ADDR  (0x48)
 
+#define SO1602A_CONFIG    (0x0C)
 #define SO1602A_I2C_ADDR  (0x3c)
 
 #define I2C_SDA_PIN  (24)
--- main.c
+++ main.c
@@ -11,16 +11,13 @@
 #include "so1602a.h"
 
 
-#define SZ(d) ((size_t)(sizeof(d)/sizeof(d[0])))
-
-
 static void system_init()
 {
     stdio_init_all();
 
     i2c_setup(i2c0, I2C_SDA_PIN, I2C_SCL_PIN, 400000);
     adt7410_set_config(i2c0, ADT7410_I2C_ADDR, ADT7410_CONFIG);
-    so1602a_setup(i2c0, SO1602A_I2C_ADDR, SO1602A_DEFAULT);
+    so1602a_setup(i2c0, SO1602A_I2C_ADDR, SO1602A_CONFIG);
 }
 
 int main(void)
@@ -37,7 +34,6 @@ int main(void)
         //
         so1602a_set_position(i2c0, SO1602A_I2C_ADDR, 0x00);
         so1602a_send_data(i2c0, SO1602A_I2C_ADDR, (uint8_t *)"Temperature:", 12);
-
         so1602a_set_position(i2c0, SO1602A_I2C_ADDR, 0x28);
         so1602a_send_data(i2c0, SO1602A_I2C_ADDR, buf, (size_t)strlen(buf));
         sleep_ms(1000);

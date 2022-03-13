5acc30752015b3dbc4e1d8e288c05c668e253c50
Ohshima Yusuke <ohshima.yusuke@yukke.org>
2022-03-13 22:23:43 +0900

    Improve adt7410 module

--- adt7410.c
+++ adt7410.c
@@ -1,14 +1,35 @@
+/**
+ * License: MIT License
+ */
+#include <stdlib.h>
+
 #include "adt7410.h"
 #include "i2c_util.h"
 
 
-double adt7410_get_temprature(i2c_inst_t *i2c, uint8_t i2c_addr)
-{
+struct adt7410_t *adt7410_open(i2c_inst_t *i2c, uint8_t i2c_addr) {
+    struct adt7410_t *dev = NULL;
+
+    dev = (struct adt7410_t *)malloc(sizeof(struct adt7410_t));
+    dev->i2c = i2c;
+    dev->addr = i2c_addr;
+
+    return dev;
+}
+
+void adt7410_close(struct adt7410_t *dev) {
+    if(dev != NULL) {
+        free(dev);
+        dev = NULL;
+    }
+}
+
+double adt7410_get_temprature(struct adt7410_t *dev) {
     uint8_t buf[2] = {0};
     int32_t data = 0;
     double temp = 0.0;
 
-    i2c_readfrom_mem(i2c, i2c_addr, 0x00, buf, 2);
+    i2c_readfrom_mem(dev->i2c, dev->addr, ADT7410_REGADDR_DATA, buf, 2);
     data = (int32_t)(buf[0] << 8 | buf[1]);
     if (data > 0x8000) {
         data = data - 65536;
@@ -17,15 +38,14 @@ double adt7410_get_temprature(i2c_inst_t *i2c, uint8_t i2c_addr)
     return (double)data / 128.0;
 }
 
-uint8_t adt7410_get_config(i2c_inst_t *i2c, uint8_t i2c_addr)
-{
+uint8_t adt7410_get_config(struct adt7410_t *dev) {
     uint8_t buf = 0;
 
-    i2c_readfrom_mem(i2c, i2c_addr, 0x03, &buf, 1);
+    i2c_readfrom_mem(dev->i2c, dev->addr, ADT7410_REGADDR_CONFIG, &buf, 1);
+
     return buf;
 }
 
-void adt7410_set_config(i2c_inst_t *i2c, uint8_t i2c_addr, uint8_t value)
-{
-    i2c_writeto_mem(i2c, i2c_addr, 0x03, &value, 1);
+void adt7410_set_config(struct adt7410_t *dev, uint8_t value) {
+    i2c_writeto_mem(dev->i2c, dev->addr, ADT7410_REGADDR_CONFIG, &value, 1);
 }
--- adt7410.h
+++ adt7410.h
@@ -3,8 +3,21 @@
 
 #include "hardware/i2c.h"
 
-double adt7410_get_temprature(i2c_inst_t *i2c, uint8_t i2c_addr);
-uint8_t adt7410_get_config(i2c_inst_t *i2c, uint8_t i2c_addr);
-void adt7410_set_config(i2c_inst_t *i2c, uint8_t i2c_addr, uint8_t value);
+#define ADT7410_REGADDR_DATA_MSB    (0x00)
+#define ADT7410_REGADDR_DATA_LSB    (0x01)
+#define ADT7410_REGADDR_DATA        (ADT7410_REGADDR_DATA_MSB)
+#define ADT7410_REGADDR_CONFIG      (0x03)
+
+struct adt7410_t {
+    i2c_inst_t *i2c;
+    uint8_t addr;
+};
+
+struct adt7410_t *adt7410_open(i2c_inst_t *i2c, uint8_t i2c_addr);
+void adt7410_close(struct adt7410_t *dev);
+
+double adt7410_get_temprature(struct adt7410_t *dev);
+uint8_t adt7410_get_config(struct adt7410_t *dev);
+void adt7410_set_config(struct adt7410_t *dev, uint8_t value);
 
 #endif

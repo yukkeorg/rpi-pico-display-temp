8abaab773b3cc6e66ca0199a240d20ca771266f5
Ohshima Yusuke <ohshima.yusuke@yukke.org>
2022-03-13 22:54:38 +0900

    Improved so1602a module

--- so1602a.c
+++ so1602a.c
@@ -1,41 +1,56 @@
+#include <stdlib.h>
 #include <stdint.h>
 
-#include "pico/stdlib.h"
 #include "hardware/i2c.h"
 #include "so1602a.h"
 
 
-void so1602a_set_command(i2c_inst_t *i2c, uint8_t i2c_addr, uint8_t command)
-{
+struct so1602a_t *so1602a_open(i2c_inst_t *i2c, uint8_t i2c_addr) {
+    struct so1602a_t *dev = NULL;
+
+    dev = (struct so1602a_t *)malloc(sizeof(struct so1602a_t));
+
+    dev->i2c = i2c;
+    dev->addr = i2c_addr;
+
+    return dev;
+}
+
+void so1602a_close(struct so1602a_t *dev) {
+    if(dev != NULL) {
+        free(dev);
+        dev = NULL;
+    }
+}
+
+void so1602a_set_command(struct so1602a_t *dev, uint8_t command) {
     uint8_t buf[2] = {0x00};
     buf[0] = 0x00;
     buf[1] = command;
-    i2c_write_blocking(i2c, i2c_addr, buf, 2, false);
+    i2c_write_blocking(dev->i2c, dev->addr, buf, 2, false);
 }
 
-void so1602a_send_data(i2c_inst_t *i2c, uint8_t i2c_addr, uint8_t *data, size_t len)
-{
+void so1602a_send_data(struct so1602a_t *dev, uint8_t *data, size_t len) {
     uint8_t buf[2] = {0x00};
     for(size_t i = 0; i < len; i++) {
         buf[0] = 0x40;
         buf[1] = data[i];
-        i2c_write_blocking(i2c, i2c_addr, buf, 2, false);
+        i2c_write_blocking(dev->i2c, dev->addr, buf, 2, false);
     }
 }
 
-void so1602a_set_position(i2c_inst_t *i2c, uint8_t i2c_addr, uint8_t addr) {
-    so1602a_set_command(i2c, i2c_addr, 0x80|addr);
+void so1602a_set_position(struct so1602a_t *dev, uint8_t addr) {
+    so1602a_set_command(dev, 0x80 | addr);
 }
 
-void so1602a_setup(i2c_inst_t *i2c, uint8_t i2c_addr, uint8_t config)
-{
+void so1602a_setup(struct so1602a_t *dev, uint8_t config) {
     sleep_ms(100);
-    so1602a_set_command(i2c, i2c_addr, 0x01);
+    so1602a_set_command(dev, 0x01);
     sleep_ms(20);
-    so1602a_set_command(i2c, i2c_addr, 0x02);
+    so1602a_set_command(dev, 0x02);
     sleep_ms(2);
-    so1602a_set_command(i2c, i2c_addr, config);
+    so1602a_set_command(dev, config);
     sleep_ms(2);
-    so1602a_set_command(i2c, i2c_addr, 0x01);
+    so1602a_set_command(dev, 0x01);
     sleep_ms(20);
 }
--- so1602a.h
+++ so1602a.h
@@ -8,13 +8,19 @@
 #define SO1602A_BLINK            (0x02)
 #define SO1602A_DEFAULT          (SO1602A_DISPLAY|SO1602A_CURSOR|SO1602A_BLINK)
 
+#define SO1602A_1STLINE_BEGIN_POS  (0x00)
+#define SO1602A_2NDLINE_BEGIN_POS  (0x20)
 
-void so1602a_set_command(i2c_inst_t *i2c, uint8_t i2c_addr, uint8_t command);
+struct so1602a_t {
+    i2c_inst_t *i2c;
+    uint8_t addr;
+};
 
-void so1602a_send_data(i2c_inst_t *i2c, uint8_t i2c_addr, uint8_t *buf, size_t len);
-
-void so1602a_set_position(i2c_inst_t *i2c, uint8_t i2c_addr, uint8_t addr);
-
-void so1602a_setup(i2c_inst_t *i2c, uint8_t i2c_addr, uint8_t config);
+struct so1602a_t *so1602a_open(i2c_inst_t *i2c, uint8_t i2c_addr);
+void so1602a_close(struct so1602a_t *dev);
+void so1602a_set_command(struct so1602a_t *dev, uint8_t command);
+void so1602a_send_data(struct so1602a_t *dev, uint8_t *buf, size_t len);
+void so1602a_set_position(struct so1602a_t *dev, uint8_t addr);
+void so1602a_setup(struct so1602a_t *dev, uint8_t config);
 
 #endif
